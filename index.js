68d573d418c31ac7cf33ff459efc228c168a2b2b
Raynaldi Zulfikar <raynaldizoel@gmail.com>
2023-06-19 15:00:29 +0700

    Add function hitung

--- index.js
+++ index.js
@@ -37,24 +37,55 @@ client.on('ready', () => {
 
 
 function man() {
+  let shoppingList = [];
+  let harga = [];
+  
   client.on('message_create', async (message) => {
     if (message.body.includes('!everyone')) {
-
+      message.reply('OpenAI Limit GAN!');
     } else if (message.body.includes('!ping')) {
-
-    } else if (message.body.startsWith('!hitung')) {
-      // Handle the !calc command (calculator)
-      const expression = message.body.split('!hitung')[1].trim();
-      try {
-        const result = eval(expression);
-        const resultFormatted = formatRupiah(result);
-        message.reply(`Hasil: ${resultFormatted}`);
-      } catch (error) {
-        message.reply('Terjadi kesalahan dalam perhitungan.');
+      message.reply('Ping!');
+    } 
+    // else if (message.body.startsWith('!hitung')) {
+    //   // Handle !hitung command
+    //   const expression = message.body.split('!hitung')[1].trim();
+    //   try {
+    //     const result = eval(expression);
+    //     const resultFormatted = formatRupiah(result);
+    //     message.reply(`Hasil: ${resultFormatted}`);
+    //   } catch (error) {
+    //     message.reply('Terjadi kesalahan dalam perhitungan.');
+    //   }
+    // } 
+    else if (message.body.startsWith('!list')) {
+      // Handle !list command
+      const input = message.body.split('!list')[1].trim();
+      const items = input.split(',');
+      items.forEach((item) => {
+        const [name, price] = item.split(':');
+        shoppingList.push(name.trim());
+        harga.push(parseFloat(price.trim()));
+      });
+      message.reply(`Berhasil ditambahkan ke dalam list.`);
+    } else if (message.body === '!hitung') {
+      // Handle !hitung command
+      if (shoppingList.length === 0) {
+        message.reply('List masih kosong.');
+      } else {
+        const formattedList = shoppingList.map((item, index) => `${index + 1}. ${item} - ${formatRupiah(harga[index])}`);
+        const listMessage = `List:\n\n${formattedList.join('\n')}`;
+        const totalPrice = harga.reduce((total, price) => total + price, 0);
+        const totalPriceFormatted = formatRupiah(totalPrice);
+        const messageWithTotalPrice = `${listMessage}\n\nTotal : ${totalPriceFormatted}`;
+        message.reply(messageWithTotalPrice);
       }
-    }
+    }else if (message.body === '!clear') {
+      // Handle !clear command to clear all arrays
+      shoppingList = [];
+      harga = [];
+      message.reply(`Semua data dihapus dari list.`);
+    } 
   });
 }
 
 man();
-
